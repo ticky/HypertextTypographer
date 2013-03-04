9c3ba4ee31733e08bd04f5906ecebbc099942563
Geoff Stokes <geoff@geoffstokes.net>
2013-03-04 17:55:38 +1100

    Improving layout and naming conventions. * Making the replacement list more centralised. * Regex is built from the replacement keys. * Only one TextCommand - reduces duplication significantly.

--- hypertext_typographer.py
+++ hypertext_typographer.py
@@ -25,6 +25,22 @@ DEFAULT_MAX_FILE_SIZE = 1048576
 DEFAULT_COLOR_SCOPE_NAME = "invalid"
 DEFAULT_IS_ENABLED = True
 
+# TODO: Load from config file
+replacements = {
+    u"“": "\"",
+    u"”": "\"",
+    u"‘": "'",
+    u"’": "'",
+    u"…": "...",
+    u"‒": "-",
+    u"–": "-",
+    u"—": "-",
+    u"―": "-",
+    u"½": "1/2",
+    u"¼": "1/4",
+    u"¾": "3/4"
+}
+
 #Set whether the plugin is on or off
 ts_settings = sublime.load_settings('hypertext_typographer.sublime-settings')
 hypertext_typographer_enabled = bool(ts_settings.get('hypertext_typographer_enabled', DEFAULT_IS_ENABLED))
@@ -36,25 +52,41 @@ def is_find_results(view):
 def is_hypertext_type(view):
     return (view.settings().get('syntax') and ("HTML" in view.settings().get('syntax') or "XML" in view.settings().get('syntax')) or view.file_name() != None and "aspx" in view.file_name())
 
-# Return an array of regions matching trailing spaces.
-def find_hypertext_typographer(view):
-    return view.find_all(u'[“”‘’…‒–—―½¼¾]')
+# Return an array of regions matching problematic characters.
+def find_problem_characters(view):
+    return view.find_all(u'[%s]' % "".join(replacements.keys()))
 
-
-# Highlight trailing spaces
-def highlight_hypertext_typographer(view):
+# Highlight problematic characters
+def highlight_problem_characters(view):
     max_size = ts_settings.get('hypertext_typographer_file_max_size', DEFAULT_MAX_FILE_SIZE)
     color_scope_name = ts_settings.get('hypertext_typographer_highlight_color', DEFAULT_COLOR_SCOPE_NAME)
     if view.size() <= max_size and is_hypertext_type(view) and not is_find_results(view):
-        regions = find_hypertext_typographer(view)
+        regions = find_problem_characters(view)
         view.add_regions('HypertextTypographerHighlightListener', regions, color_scope_name, sublime.DRAW_EMPTY)
 
 
-# Clear all trailing spaces
-def clear_hypertext_typographer_highlight(window):
+# Clear all highlights
+def highlight_clear(window):
     for view in window.views():
         view.erase_regions('HypertextTypographerHighlightListener')
 
+# --- Event Listeners ---
+
+# Highlight matching regions.
+class HypertextTypographerHighlightListener(sublime_plugin.EventListener):
+    def on_modified(self, view):
+        if hypertext_typographer_enabled:
+            highlight_problem_characters(view)
+
+    def on_activated(self, view):
+        if hypertext_typographer_enabled:
+            highlight_problem_characters(view)
+
+    def on_load(self, view):
+        if hypertext_typographer_enabled:
+            highlight_problem_characters(view)
+
+# --- Commands ---
 
 # Toggle the event listner on or off
 class ToggleTypographyHighlightCommand(sublime_plugin.WindowCommand):
@@ -65,31 +97,16 @@ class ToggleTypographyHighlightCommand(sublime_plugin.WindowCommand):
         # If toggling on, go ahead and perform a pass,
         # else clear the highlighting in all views
         if hypertext_typographer_enabled:
-            highlight_hypertext_typographer(self.window.active_view())
+            highlight_problem_characters(self.window.active_view())
         else:
-            clear_hypertext_typographer_highlight(self.window)
+            highlight_clear(self.window)
 
 
-# Highlight matching regions.
-class HypertextTypographerHighlightListener(sublime_plugin.EventListener):
-    def on_modified(self, view):
-        if hypertext_typographer_enabled:
-            highlight_hypertext_typographer(view)
-
-    def on_activated(self, view):
-        if hypertext_typographer_enabled:
-            highlight_hypertext_typographer(view)
-
-    def on_load(self, view):
-        if hypertext_typographer_enabled:
-            highlight_hypertext_typographer(view)
-
-
-# HTML Escape matching regions.
-# This will replace any and all characters with HTML/XML equivalent character references.
+# Either Replace matching regions with plain replacements or HTML Escape matching regions.
 class EscapeTypographyCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        regions = find_hypertext_typographer(self.view)
+    def run(self, edit, mode="replace"):
+        global replacements
+        regions = find_problem_characters(self.view)
         if regions:
             # replacing a region changes the other regions positions, so we
             # handle this maintaining an offset
@@ -97,7 +114,11 @@ class EscapeTypographyCommand(sublime_plugin.TextCommand):
             for region in regions:
                 r = sublime.Region(region.a + offset, region.b + offset)
                 t = self.view.substr(r)
-                tr = t.encode('ascii','xmlcharrefreplace')
+                tr = t
+                if mode == "escape":
+                    tr = t.encode('ascii','xmlcharrefreplace')
+                elif mode == "replace":
+                    tr = "".join(replacements.get(c,c) for c in t)
                 self.view.replace(edit, r, tr)
                 offset += len(tr) - r.size()
 
@@ -108,43 +129,3 @@ class EscapeTypographyCommand(sublime_plugin.TextCommand):
             msg = "No special characters to escape!"
 
         sublime.status_message(msg)
-
-
-# Replace matching regions with plain replacements.
-class ReplaceTypographyCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        replacements = {
-            u"“": "\"",
-            u"”": "\"",
-            u"‘": "'",
-            u"’": "'",
-            u"…": "...",
-            u"‒": "-",
-            u"–": "-",
-            u"—": "-",
-            u"―": "-",
-            u"½": "1/2",
-            u"¼": "1/4",
-            u"¾": "3/4"
-        }
-        regions = find_hypertext_typographer(self.view)
-        if regions:
-            # replacing a region changes the other regions positions, so we
-            # handle this maintaining an offset
-            offset = 0
-            for region in regions:
-                r = sublime.Region(region.a + offset, region.b + offset)
-                t = self.view.substr(r)
-                tr = "".join(replacements.get(c,c) for c in t)
-                sublime.status_message(tr)
-                self.view.replace(edit, r, tr)
-                offset += len(tr) - r.size()
-
-            msg_parts = {"nbRegions": len(regions),
-                         "plural":    's' if len(regions) > 1 else ''}
-            msg = "Replaced %(nbRegions)s special character%(plural)s" % msg_parts
-        else:
-            msg = "No special characters to replace!"
-
-        sublime.status_message(msg)
-
